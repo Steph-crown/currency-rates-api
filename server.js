dfa4b1ff6f04a789b89d5b08023b029a70bb6ce2
Steph-crown <emmanuelstephen024@gmail.com>
2021-01-09 08:37:39 +0100

    Made final changes to the server.js

--- server.js
+++ server.js
@@ -3,32 +3,31 @@ const bodyParser = require('body-parser');
 const https = require('https');
 
 const app = express();
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded( {extended: false} ));
 app.use(bodyParser.json());
 
 app.get('/api/rates', (req, res) => {
     const {base, currency} = req.query;
     if (!base) res.status(400).json({
-        error: "`base` query parameter string has no value"
+        error: "`base` query parameter is not defined or has no value"
     })
     else if (!currency) res.status(400).json({
-        error: "`currency` query parameter string has no value"
+        error: "`currency` query parameter is not defined or has no value"
     })
     else {
         let url = `https://api.exchangeratesapi.io/latest?base=${base}&symbols=${currency}`
-        https.get(url, result => {
-            let data = ""
-            result.on("data", d => {
-                data += d
+        https.get(url, response => {
+            let result = ""
+            response.on("data", d => {
+                result += d
             })
-            result.on("end", () => {
-                data = JSON.parse(data)
-                if (data.error) {
-                    res.status(400).json(data)
-                    console.log(data)
+            response.on("end", () => {
+                result = JSON.parse(result)
+                if (result.error) {
+                    res.status(400).json(result)
                 }
                 else {
-                    res.status(200).json(data)
+                    res.status(200).json({result})
                 }
             })
         })
